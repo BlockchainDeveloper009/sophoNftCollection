a06b4b718190bb336bbcc5d732e89616b4e2d4c9
alpha@DESKTOP-6F6AEHR <graphmywork@gmail.com>
2022-05-03 20:09:26 -0500

    pushing website & backend project

---
+++ sophoNftCollectionbk/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x6c82eFF78eF79Bc45EC286557D5c937a547d6Bc4";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
