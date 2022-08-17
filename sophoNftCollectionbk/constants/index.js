d3d5fa328956393a5f8c59dac35f32251c860733
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2022-08-17 18:39:16 -0500

    replace METADATA_URL with my vercelapp api link

--- sophoNftCollectionbk/constants/index.js
+++ sophoNftCollectionbk/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x6c82eFF78eF79Bc45EC286557D5c937a547d6Bc4";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
+const METADATA_URL = "https://sophonftcollection-frontend-vercel-v1-2.vercel.app/api/";
+//const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ sophoNftCollectionbk/nftCryptoDevsConstant.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x65A388e7D7432B2333f6d734DB961CFFd17d8aB2";//"0xF85e86E6Caf61407a20c6Be258fD97a76407a181";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://sophonftcollection-frontend-vercel-v1-2.vercel.app/api/";
+//const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+//
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
